6bf796037afd96582e21d031ebf6203e423909d9
Akash47a <sakash0120@gmail.com>
2026-07-15 22:08:49 +0530

    updated2 sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SideBar() {
   return (
-    <div>
+    <div className='h-100 d-flex flex-column justify-content-center align-items-center gap-3'>
       <div ><img src="src\assets\profile1.jpg" alt="" className='rounded-circle' style={{width:"100px"}}/></div>
       <div>DashBoard</div>
       <div>Projects</div>
