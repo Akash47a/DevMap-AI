c0ac10225fa23b80ea6a6abfb45feabb07c42d49
Akash47a <sakash0120@gmail.com>
2026-07-15 21:53:58 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Contant from './Contant'
+import NavBar from './NavBar'
+import SideBar from './SideBar'
 function App() {
   return (
-    <div>App</div>
+    <div className='d-flex '>
+      <div style={{width:"20%", height:"100vh"}}><SideBar/></div>
+      <div style={{width:"80%"}}>
+        <div style={{height:"50px"}}><NavBar/></div>
+        <div><Contant/></div>
+      </div>
+    </div>
   )
 }
 
