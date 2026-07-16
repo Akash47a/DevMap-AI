33a13474613399adf614aebb0da487516b613301
Akash47a <sakash0120@gmail.com>
2026-07-16 21:25:15 +0530

    updated dashboard

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function DashBoard() {
   return (
-    <div>DashBoard</div>
+    <div>
+      <div className='DashBoard-center'>
+        <div className='welcome'></div>
+        <div className='Projects-home'>
+          <div>Project</div>
+          <div className='project-box'></div>
+        </div>
+        <div className='Quick-links'>
+          <div>Quick access</div>
+          <div className='links'></div>
+        </div>
+        <div className='Recent-tabs'>
+
+        </div>
+      </div>
+      <div className='DashBoard-right'>
+        <div className='Calendar-home'>
+
+        </div>
+        <div className='Tasks-home'>
+
+        </div>
+        <div className='Progress-home'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
