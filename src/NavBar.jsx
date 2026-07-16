ce25a042c0da9b8d2fdb1647fe3282bf7784cd28
Akash47a <sakash0120@gmail.com>
2026-07-16 21:11:48 +0530

    updated navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='d-flex justify-content-between'>
+      <div>Page Name</div>
+      <div className='d-flex gap-5'>
+        <div>Search</div>
+        <div className='d-flex'>
+          <div><img src="src\assets\icons8-fire-48.png" alt="" className='w-50' /></div>
+          <div><img src="src\assets\icons8-notification-48.png" alt="" className='w-50'/></div>
+          <div><img src="src\assets\icons8-moon-48.png" alt="" className='w-50'/></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
